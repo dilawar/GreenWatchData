c38452052defda72b7edfa4e0c6be9a0ad2fbb5d
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-10 11:56:11 +0530

    Download both ca-land and diverted land.

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -x
 set -e
-for state in 'Andhra Pradesh' 'Telangana' 'Karanataka' \
-    'Rajasthan' 'Gujarat' 'Maharashtra' 'Madhya Pradesh' \
-    'Tamil Nadu'; do
-    python3 ./download_all_kml_selenium.py "$state" 'CA Land'
+for catype in 'CA Land' 'Diverted Land'; do
+    for state in 'Andhra Pradesh' 'Telangana' 'Karanataka' \
+        'Rajasthan' 'Gujarat' 'Maharashtra' 'Madhya Pradesh' \
+        'Tamil Nadu'; do
+        python3 ./download_all_kml_selenium.py "$state" "$catype"
+    done
 done
