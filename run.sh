351006c9bbeb065acae56b6b6866a555273b3e6e
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-10 14:43:42 +0530

    Added more data.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 for catype in 'CA Land' 'Diverted Land'; do
-    for state in 'Andhra Pradesh' 'Telangana' 'Karanataka' \
+    for state in 'Andhra Pradesh' 'Telangana' 'Karnataka' \
         'Rajasthan' 'Gujarat' 'Maharashtra' 'Madhya Pradesh' \
         'Tamil Nadu'; do
         python3 ./download_all_kml_selenium.py "$state" "$catype"
